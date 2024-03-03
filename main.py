f292b0019308152e5d8e399b49af40dde8ad921d
Bassam <bassamtop123@gmail.com>
2024-03-03 22:47:11 +0300

    - fixed issue with old checking and colour highlighting system - added spacing for better readability

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-
 
 def import_words_to_array(filepath: str) -> list:
     words = open(filepath, "r")
@@ -7,6 +5,7 @@ def import_words_to_array(filepath: str) -> list:
     for word in words:
         wordlist.append(word[:-1])
     return wordlist
+
 
 
 def guessing(valid_guesses: list[str], answer: str) -> bool:
@@ -20,23 +19,20 @@ def guessing(valid_guesses: list[str], answer: str) -> bool:
         for guess_letters in guesses:
             print(' '.join(guess_letters))
         guess = input('Guess: ')
+        
         while guess not in valid_guesses:  # Check 1: Word is valid english 5-letter word refers to list in words.txt
             guess = input('Invalid Word, Try again: ')
         guess_letters = list(guess)
-        for x in range(0, 5):
-            if guess_letters[x] == (list(answer))[x]:  # Check 2: if letter in guess same pos as in answer, green
-                guess_letters[x] = f'{green}{guess_letters[x]}{color_end}'
-            elif guess_letters[x] in list(answer):  # Check 3: if letter in guess even exists in answer, yellow
+        ans_temp = list(answer)
+        
+        for x in range(0, 5): # Letter highlighting 
+            if guess_letters[x] == ans_temp[x]: # Green if in same pos as answer
+               guess_letters[x] = f'{green}{guess_letters[x]}{color_end}'
+               ans_temp[x] = ''
+            elif guess_letters[x] in ans_temp: # Yellow if in answer but not same position
+                pos_of_guess_letter = ans_temp.index(guess_letters[x])
                 guess_letters[x] = f'{yellow}{guess_letters[x]}{color_end}'
-        for letter in guess:  # Check 4: gets the best result when duplicate letters pass previous checks
-            if len(set(answer)) == len(answer):  # Check 4: Making sure duplicate letters behave as expected
-                if f'{yellow}{letter}{color_end}' in guess_letters and f'{green}{letter}{color_end}' in guess_letters:
-                    impostor_index = guess_letters.index(f'{yellow}{letter}{color_end}')
-                    guess_letters[impostor_index] = letter
-            if len(set(answer)) + 1 == len(answer) and len(set(guess)) == 3:
-                if f'{yellow}{letter}{color_end}' in guess_letters and f'{yellow}{letter}{color_end}' in guess_letters:
-                    impostor_index = guess_letters.index(f'{yellow}{letter}{color_end}')
-                    guess_letters[impostor_index] = letter
+                ans_temp[pos_of_guess_letter] = ''
         guesses.append(guess_letters)
         guess_count += 1
     if guess == answer:
@@ -49,7 +45,7 @@ def main():
     answers = import_words_to_array("answers.txt")
 
     answer = (random.choice(answers))
-#    print(f'answer(for debugging) = {answer}')  # Uncomment line for debugging
+    print(f'answer(for debugging) = {answer}')  # Uncomment line for debugging
     win = guessing(words, answer)
     if win:
         print(f'Congrats! The answer as indeed: {answer} :)')
